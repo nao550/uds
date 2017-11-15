37b71bcf6e7639ec5098de0bcf40ab225c3e337b
Naoki Ikeno <nao@kyo-to.net>
2017-11-15 14:44:50 +0900

    Added Category class files.

---
+++ lib/Cate.php
@@ -0,0 +1,98 @@
+<?php
+/*
+   public function __construct
+   public function getAllCate()
+   public function addCate( array )
+   public function getCateStr ( long )
+   public function updateCate( array )
+   public function delCate( long )
+*/
+
+namespace morris;
+use PDO;
+
+class Cate {
+  private $pdo;
+  
+  function __construct(){
+    $dsn = 'mysql:host=' . $GLOBALS['DBSV'] . ';dbname=' . $GLOBALS['DBNM'] . ';charset=utf8';
+    try{
+      $this->pdo = new PDO($dsn, $GLOBALS['DBUSER'], $GLOBALS['DBPASS']);
+    } catch (PDOException $e) {
+      exit('データベース接続失敗。'.$e->getMessage());
+    }
+  }
+
+  /* Get all Categorys
+   * @return array
+   */
+  function getAllCate()
+  {
+    $sql = "SELECT * FROM Cate;";
+    $stm = $this->pdo->query( $sql );
+    return($stm->fetchAll(PDO::FETCH_ASSOC));
+  }
+
+  /* add array
+   * @param array as added data array.
+   * @return boolen.
+  */
+  function addCate ( $Str )
+  {
+    $sql = 'INSERT INTO Cate ( nm ) VALUES (:nm );';
+    $stm = $this->pdo->prepare( $sql );
+    $stm->bindValue(':nm', $Str, PDO::PARAM_STR);
+    return ($stm->execute());
+  }
+
+  function getCateStr( $cd ){
+    $sql = "SELECT * FROM Cate WHERE cd = :cd; ORDER BY cd ASC";
+    $stm = $this->pdo->prepare( $sql );
+    $stm->bindValue(':cd', $cd, PDO::PARAM_INT);
+    $stm->execute();
+    $row = $stm->fetch(PDO::FETCH_ASSOC);
+    return $row;
+  }
+  
+
+  function updateCate( $arStr ){
+    $sql = 'UPDATE Cate SET nm = :nm where cd = :cd ;';
+    $stm = $this->pdo->prepare( $sql );
+    $stm->bindValue(':cd', $arStr['cd'], PDO::PARAM_INT);
+    $stm->bindValue(':nm', $arStr['nm'], PDO::PARAM_STR);        
+    $stm->execute();
+    return true;
+  }
+
+  function delCate( $cd )
+  {
+    $sql = "DELETE FROM Cate Where cd = :cd;";
+    $stm = $this->pdo->prepare( $sql );
+    $stm->bindValue(':cd', $cd, PDO::PARAM_INT);
+    return ( $stm->execute());
+  }
+
+
+  /* getSelectedValue for test function.
+   * @param $cd int as Qst cd.
+   * @param $str string as field.
+   * @return string field value.
+  */
+  function getSelectedValue ( $cd, $str )
+  {
+    $sql = "SELECT ".$str." FROM Cate WHERE cd = ".$cd.";";
+    $stm = $this->pdo->prepare($sql);
+    $stm->execute();
+    $row = $stm->fetch(PDO::FETCH_ASSOC);
+    return $row[$str];
+  }
+  
+
+  /* return last inserted id
+   * @return string
+   */
+  function getLastinsertId (){
+    return $this->pdo->lastInsertId();
+  }
+  
+}
---
+++ tests/CateTest.php
@@ -0,0 +1,70 @@
+<?php
+use morris\Cate;
+
+require_once __DIR__.'/Generic_Tests_DatabaseTestCase.php';
+
+class RespTest extends Generic_Tests_DatabaseTestCase
+{
+  private $cd;
+  private $nm;
+  
+  
+/*
+  public function setUp(){
+  }
+  */
+  /**
+  * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+  **/
+  public function getDataSet()
+  {
+    $this->cd = 99;
+    $this->nm = 'カテゴリ';
+    $this->arStr = array('cd' => $this->cd, 'nm' => $this->nm );
+    $this->target = new Cate;
+
+    return $this->createFlatXMLDataSet(dirname(__FILE__).'/_files/CateDB.xml');    
+  }
+
+ public function testGetAllCate()
+  {
+    // 初期登録データの件数チェック
+    $n = 0;
+    foreach ( $this->target->getAllCate() as $row ){
+      $n++;
+    }
+    $this->assertEquals(2, $n);
+  }
+  
+  public function testGetRowCount()
+  {
+    $this->assertEquals(2, $this->getConnection()->getRowCount( 'Cate'));
+  }
+
+  public function testAddCate ()
+  {
+    $this->assertEquals(1, $this->target->addCate( $this->nm) );
+    $cd = $this->target->getLastInsertId();
+    $this->assertEquals($this->nm, $this->target->getSelectedValue( $cd, 'nm' ));
+    $this->assertEquals(3, $this->getConnection()->getRowCount( 'Cate'));
+  }
+  
+  public function testUpdateCate()
+  {
+    $this->target->addCate( $this->nm );
+    $this->arStr['cd'] = $this->target->getlastinsertId();
+    $this->arStr['nm'] = 'カテゴリごり';
+    $this->assertTrue( $this->target->UpdateCate( $this->arStr ));
+    $this->assertEquals($this->arStr['nm'], $this->target->getSelectedValue($this->arStr['cd'], 'nm'));
+  }
+
+  public function testDelCate()
+  {
+    $this->target->addCate( $this->nm);
+    $this->arStr['cd'] = $this->target->getlastinsertId();
+    $this->assertTrue( $this->target->delCate( $this->arStr['cd'] ));
+  }
+
+}
+
+
