43a19702d9b082ba4b5f4b1c1490c0025da79259
Naoki Ikeno <nao@kyo-to.net>
2018-01-10 16:13:56 +0900

    Modify top instractions.

--- lib/Exam.php
+++ lib/Exam.php
@@ -1,11 +1,13 @@
 <?php
 /*
    public function __construct
-   public function GetAllExam()
-   public function AddExam( array )
-   public function GetExamStr ( long )
-   public function UpdateExam( array )
-   public function DelExam( long )
+   public function arStrFmt( string )
+   public function getAllExam()
+   public function addExam( array )
+   public function getExamStr ( long )
+   public function updateExam( array )
+   public function delExam( long )
+   public function countExam(){
 
 
 */
@@ -15,7 +17,7 @@ use PDO;
 
 class Exam {
   private $pdo;
-  
+
   function __construct(){
     $dsn = 'mysql:host=' . $GLOBALS['DBSV'] . ';dbname=' . $GLOBALS['DBNM'] . ';charset=utf8';
     try{
@@ -25,7 +27,7 @@ class Exam {
     }
   }
 
-  /* 
+  /*
    */
   function arStrFmt( $arStr ){
     // 受けとった arStr はpostなのでサニタイズする必要がある
@@ -35,7 +37,7 @@ class Exam {
       'catecd' => FILTER_VALIDATE_INT,
       'type' => FILTER_VALIDATE_INT,
       'exam' => FILTER_DEFAULT,
-      'correct' => FILTER_VALIDATE_INT,      
+      'correct' => FILTER_VALIDATE_INT,
       'ans1' => FILTER_DEFAULT,
       'ans2' => FILTER_DEFAULT,
       'ans3' => FILTER_DEFAULT,
@@ -47,7 +49,7 @@ class Exam {
 
     return $arExam;
   }
-  
+
   /* Get all exams
    * @return array
    */
@@ -66,13 +68,13 @@ class Exam {
   {
 
     $arExam = $this->arStrFmt( $arStr );
-    
+
     $sql = 'INSERT INTO Exam (catecd, type, exam, correct, ans1, ans2, ans3, ans4, ans5, regdate ) VALUES (:catecd, :type, :exam, :correct, :ans1, :ans2, :ans3, :ans4, :ans5, NOW());';
     $stm = $this->pdo->prepare( $sql );
     $stm->bindValue(':catecd', $arExam['catecd'], PDO::PARAM_INT);
-    $stm->bindValue(':type', $arExam['type'], PDO::PARAM_INT);    
+    $stm->bindValue(':type', $arExam['type'], PDO::PARAM_INT);
     $stm->bindValue(':exam', $arExam['exam'], PDO::PARAM_STR);
-    $stm->bindValue(':correct', $arExam['correct'], PDO::PARAM_INT); 
+    $stm->bindValue(':correct', $arExam['correct'], PDO::PARAM_INT);
     $stm->bindValue(':ans1', $arExam['ans1'], PDO::PARAM_STR);
     $stm->bindValue(':ans2', $arExam['ans2'], PDO::PARAM_STR);
     $stm->bindValue(':ans3', $arExam['ans3'], PDO::PARAM_STR);
@@ -89,25 +91,25 @@ class Exam {
     $row = $stm->fetch(PDO::FETCH_ASSOC);
     return $row;
   }
-  
+
 
   function updateExam( $arStr ){
 
     $arExam = $this->arStrFmt( $arStr );
-    
+
     $sql = 'UPDATE Exam SET catecd = :catecd, type = :type, exam = :exam, '.
            'correct = :correct, ans1 = :ans1, ans2 = :ans2, ans3 = :ans3, '.
            'ans4 = :ans4, ans5 = :ans5 where cd = :cd ;';
     $stm = $this->pdo->prepare( $sql );
     $stm->bindValue(':catecd', $arExam['catecd'], PDO::PARAM_INT);
-    $stm->bindValue(':type', $arExam['type'], PDO::PARAM_INT);        
+    $stm->bindValue(':type', $arExam['type'], PDO::PARAM_INT);
     $stm->bindValue(':exam', $arExam['exam'], PDO::PARAM_STR);
-    $stm->bindValue(':correct', $arExam['correct'], PDO::PARAM_INT);        
-    $stm->bindValue(':ans1', $arExam['ans1'], PDO::PARAM_STR);    
-    $stm->bindValue(':ans2', $arExam['ans2'], PDO::PARAM_STR);    
-    $stm->bindValue(':ans3', $arExam['ans3'], PDO::PARAM_STR);    
-    $stm->bindValue(':ans4', $arExam['ans4'], PDO::PARAM_STR);    
-    $stm->bindValue(':ans5', $arExam['ans5'], PDO::PARAM_STR);    
+    $stm->bindValue(':correct', $arExam['correct'], PDO::PARAM_INT);
+    $stm->bindValue(':ans1', $arExam['ans1'], PDO::PARAM_STR);
+    $stm->bindValue(':ans2', $arExam['ans2'], PDO::PARAM_STR);
+    $stm->bindValue(':ans3', $arExam['ans3'], PDO::PARAM_STR);
+    $stm->bindValue(':ans4', $arExam['ans4'], PDO::PARAM_STR);
+    $stm->bindValue(':ans5', $arExam['ans5'], PDO::PARAM_STR);
     $stm->bindValue(':cd', $arExam['cd'], PDO::PARAM_INT);
     $stm->execute();
     return true;
@@ -126,10 +128,10 @@ class Exam {
     $stm = $this->pdo->query( $sql );
     return $stm->fetchColumn(0);
   }
- 
+
 
   /* for test */
-  
+
   /* getSelectedValue for test function.
    * @param $cd int as Exam cd.
    * @param $str string as field.
@@ -150,5 +152,5 @@ class Exam {
   function getLastinsertId (){
     return $this->pdo->lastInsertId();
   }
-  
+
 }
